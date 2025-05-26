1f6bd318bef92765532cd20d2661cf7db910d693
UTSAV SINGHAL <utsavsinghal26@gmail.com>
2025-05-26 13:09:08 +0000

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   base: '/cluster/',
+  plugins: [react(), tailwindcss()],
 });
