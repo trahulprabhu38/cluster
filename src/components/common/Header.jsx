5b6a942621d35aa3530ab8a710dbc271d0b39c8b
UTSAV SINGHAL <utsavsinghal26@gmail.com>
2025-05-26 18:43:46 +0530

    Update Header.jsx

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -41,7 +41,6 @@ const Header = () => {
                             <h1 className="text-xl font-bold bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent">
                                 CLUSTER
                             </h1>
-                            <p className="text-xs text-slate-400 leading-none">Data Excellence</p>
                         </div>
                     </div>
 
